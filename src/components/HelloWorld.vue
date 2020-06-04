d9a93ddf0d84311c6d234c93918d3585d4833f8f
SugarBooty <10579461+SugarBooty@users.noreply.github.com>
2020-06-04 11:18:51 -0400

    Employees added

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,9 +2,9 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      If you are reading this you are gay!<br>
+      Bigfoot exists 
+      <a href="https://www.bigfootproof.com/" target="_blank" rel="noopener">here is proof!</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
     <ul>
